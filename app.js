c56f00a3a26146c5c7ed1d8e7cc20bd84aff17b9
verozroke <73778760+verozroke@users.noreply.github.com>
2022-07-10 20:21:14 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// document.getElementById('nav').onmouseover = event => {
+//     const target = event.target;
+//     if (target.className === 'menu-item') {
+//         const s = target.getElementsByClassName('submenu');
+//         closeMenu();
+//         s[0].style.display = 'block';
+//     }
+// }
+//
+//
+
+
+console.log('My name is Askhat')
