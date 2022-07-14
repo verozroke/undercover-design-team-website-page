b7e55945a71cb56127cb62f508b78b1f8b1c0499
verozroke <73778760+verozroke@users.noreply.github.com>
2022-07-14 17:28:17 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-// document.getElementById('nav').onmouseover = event => {
-//     const target = event.target;
-//     if (target.className === 'menu-item') {
-//         const s = target.getElementsByClassName('submenu');
-//         closeMenu();
-//         s[0].style.display = 'block';
+const anchors = document.querySelectorAll('a[href*="#"]')
+// const hentaiMusic = new Audio('audio/hentai.mp3')
+
+anchors.forEach(anchor => {
+    anchor.addEventListener('click', event => {
+        event.preventDefault()
+        const blockID = anchor.getAttribute('href').substring(1)
+        console.log(document.getElementById(blockID))
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}) 
+
+
+const scrollForMore = document.getElementById('scroll-button') 
+
+scrollForMore.addEventListener('click', event => {
+    event.preventDefault()
+    const blockID = document.getElementById('services')
+    blockID.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+})
+
+// let clicksToLamp = 0
+// const lampochka = document.getElementById('lampochka')
+
+// lampochka.addEventListener('click', event => {
+//     clicksToLamp+= 1
+//     console.log('Пасхалочка через: ' + (10-clicksToLamp))
+//     if (clicksToLamp === 10) {
+//         alert('Hentai Time!~~~')
+//         hentaiMusic.play()
+//         clicksToLamp = 0
 //     }
-// }
-//
-//
-
-
-console.log('My name is Askhat')
+// })
