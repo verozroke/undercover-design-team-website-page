b9b682eb4534fff4e10299a7bcbb0b88db2fb61d
verozroke <73778760+verozroke@users.noreply.github.com>
2022-07-14 22:09:45 +0600

    Complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const anchors = document.querySelectorAll('a[href*="#"]')
-// const hentaiMusic = new Audio('audio/hentai.mp3')
+const hentaiMusic = new Audio('audio/hentai.mp3')
 
 anchors.forEach(anchor => {
     anchor.addEventListener('click', event => {
@@ -25,15 +25,15 @@ scrollForMore.addEventListener('click', event => {
     })
 })
 
-// let clicksToLamp = 0
-// const lampochka = document.getElementById('lampochka')
+let clicksToLamp = 0
+const lampochka = document.getElementById('lampochka')
 
-// lampochka.addEventListener('click', event => {
-//     clicksToLamp+= 1
-//     console.log('Пасхалочка через: ' + (10-clicksToLamp))
-//     if (clicksToLamp === 10) {
-//         alert('Hentai Time!~~~')
-//         hentaiMusic.play()
-//         clicksToLamp = 0
-//     }
-// })
+lampochka.addEventListener('click', event => {
+    clicksToLamp+= 1
+    console.log('Пасхалочка через: ' + (100-clicksToLamp))
+    if (clicksToLamp === 100) {
+        alert('Hentai Time!~~~')
+        hentaiMusic.play()
+        clicksToLamp = 0
+    }
+})
